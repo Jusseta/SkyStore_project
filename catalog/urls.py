a9eb82396ce86f616d8652f22385bb0e7ea0747c
Victoria Kashpur <vikki0011@gmail.com>
2023-09-10 03:33:55 +1000

    added blog, changed FBV to CBV, CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, kinds, flowers, flower_page
+from catalog.views import HomeView, contacts, CategoryListView, ProductListView, ProductsDetailView, BlogListView, \
+    BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,9 +9,14 @@ from django.conf.urls.static import static
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('kinds/', kinds, name='kinds'),
-    path('<int:pk>/flowers/', flowers, name='flowers'),
-    path('<int:pk>/flower_info/', flower_page, name='flower_page'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('flowers/<int:pk>/', ProductListView.as_view(), name='flowers'),
+    path('flower_detail/<int:pk>/', ProductsDetailView.as_view(), name='flower_detail'),
+    path('blogs/', BlogListView.as_view(), name='blogs'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('add_blog/', BlogCreateView.as_view(), name='add_blog'),
+    path('blog/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
